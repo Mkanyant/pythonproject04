1e553eddfc1b518402e820da1e993e6985611e6f
Mkanyanat <s6519410049@sau.ac.th>
2023-09-08 11:01:22 +0700

    code v.4

---
+++ iotpython01.py
@@ -0,0 +1,15 @@
+#Function 1 : No Parameter/No  Reture
+def funcA( ) :
+    print('Wow')
+    print('Woo')
+    print('Wee')
+    print('Wea')
+    #ไม่มีคำสั่ง Reture
+
+def funcB( ) :
+    print('Hi')
+
+funcA( )
+funcA( )
+funcB( )
+funcA( )
---
+++ iotpython02.py
@@ -0,0 +1,15 @@
+#Function 2 : Have Parameter/No  Reture
+#parameter คือ ตัวแปรประเภทหนึ่ง ขอบเขตการใช้งานพารามิเตอร์
+#
+def funcA( x,y ) :
+    print('Hi..........')
+    z=x+y
+    print(f'{x} + {y} = {z}')
+    #ไม่มีคำสั่ง Reture
+
+def funcB( x ) :
+    print(f"X is {x} 555...")
+
+funcA(10,20)
+funcA(5,5)
+funcB('sau iot')
